6b7282188bb5f05a3e963adfc75127ef6ffea787
Peter Holodnak <xholodn@fi.muni.cz>
2020-02-24 17:49:57 +0100

    asd

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,14 +1,49 @@
 package cz.muni.fi.pa165.currency;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Fail.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
 
+    @Mock
+    private ExchangeRateTable exchangeRateTable;
+    private static CurrencyConvertor currencyConvertor;
+
+    private static final Currency SOURCE_CURRENCY = Currency.getInstance("MNT");
+    private static final Currency TARGET_CURRENCY = Currency.getInstance("KHR");
+
+
+    @Before
+    public void createCurrencyConvertor() {
+        currencyConvertor = new CurrencyConvertorImpl(exchangeRateTable);
+    }
+
     @Test
-    public void testConvert() {
+    public void testConvert() throws ExternalServiceFailureException {
         // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+
+        when(exchangeRateTable.getExchangeRate(SOURCE_CURRENCY, TARGET_CURRENCY))
+            .thenReturn(new BigDecimal("0.01"));
+
+//        BigDecimal actualResult = currencyConvertor.convert(SOURCE_CURRENCY, TARGET_CURRENCY, BigDecimal.valueOf(150, 2));
+//        BigDecimal expectedResult = new BigDecimal("0.02");
+//        assertThat(actualResult).isEqualTo(expectedResult);
+
+        assertThat(currencyConvertor.convert(SOURCE_CURRENCY, TARGET_CURRENCY, BigDecimal.valueOf(150, 2))).isEqualTo("0.02");
     }
 
     @Test
@@ -32,8 +67,15 @@ public class CurrencyConvertorImplTest {
     }
 
     @Test
-    public void testConvertWithExternalServiceFailure() {
-        fail("Test is not implemented yet.");
+    public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException {
+        ExternalServiceFailureException externalServiceFailureException = new ExternalServiceFailureException("Some suitable message");
+        when(exchangeRateTable.getExchangeRate(SOURCE_CURRENCY, TARGET_CURRENCY))
+            .thenThrow(externalServiceFailureException);
+
+        assertThatExceptionOfType(UnknownExchangeRateException.class)
+            .isThrownBy(() -> currencyConvertor.convert(SOURCE_CURRENCY, TARGET_CURRENCY, BigDecimal.valueOf(150, 2)))
+            .withCause(externalServiceFailureException)
+            .withMessage("Some suitable message");
     }
 
 }
